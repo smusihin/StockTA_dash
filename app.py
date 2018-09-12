a801e84041296ce2ac891fcecbd145380e9d33e7
Sergey Musikhin <musihin_sergei@mail.ru>
2018-09-12 22:38:54 +0300

    add timeframe choice

--- app.py
+++ app.py
@@ -16,19 +16,31 @@ app.title = 'Stock'
 app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
 bf = bitfinex()
 pairs = [{'label': str(n).upper(), 'value': str(n).upper()} for n in bf.getPairs()]
+timeframes = [{'label': n, 'value': n} for n in ['1m', '5m', '15m', '30m', '1h', '3h', '6h', '12h', '1D', '7D', '14D', '1M']]
 indicators = [{'label': n, 'value': n} for n in backend.get_indicator_list()]
 
 
 app.layout = html.Div([
     html.Div([
         html.Div([
-            dcc.Dropdown(
-                id='pair',
-                options=pairs,
-                value=pairs[0]['value'],
-            ),
-            dcc.Graph(id='graph')
-
+            html.Div([
+  #              html.Div([
+                    dcc.Dropdown(
+                        id='pair',
+                        options=pairs,
+                        value=pairs[0]['value'],
+                        className='nine columns'
+                    ),
+                    dcc.Dropdown(
+                        id='timeframe',
+                        options=timeframes,
+                        value=timeframes[0]['value'],
+                        className='offset-by-one two columns'
+                    )],className='row'),
+                html.Div([
+                    dcc.Graph(id='graph',className='twelve columns')
+                ],className = 'row'),
+ #           ],className='container'),
         ],className = 'nine columns'),
         html.Div([
                 dcc.Dropdown(
@@ -52,15 +64,16 @@ app.layout = html.Div([
 
 
 @app.callback(dd.Output('graph', 'figure'), [dd.Input('pair', 'value'),
+                                             dd.Input('timeframe', 'value'),
                                              dd.Input('indicator', 'value'),
                                              dd.Input('indicator_params', 'rows')])
-def update_graph(pair, indicator_name, param_rows):
+def update_graph(pair,timeframe, indicator_name, param_rows):
      indicator = backend.get_indicator(indicator_name)
      params = {}
      for r in param_rows:
          params[r['Parameter']]=r['Value']
      indicator.set_params(params)
-     df = bf.get_candles(pair, '1M').sort_index()
+     df = bf.get_candles(pair, timeframe).sort_index()
      data = [go.Scatter(x=pd.to_datetime(df.index, unit='ms'), y=df['CLOSE'], mode='lines', name=pair)]
      df_i = indicator.calculate(df['CLOSE'])
      for column in df_i.columns:
